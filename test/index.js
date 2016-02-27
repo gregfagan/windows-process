0540c9514979c897bb31609003685624c31de702
gregfagan <gregfagan@gmail.com>
2016-02-26 23:32:07 -0800

    Unify quote style in JS

--- test/index.js
+++ test/index.js
@@ -7,18 +7,18 @@ describe('window-process', () => {
   describe('WithProcess', () => {
     it('should expect a string and a callback as arguments', () => {
       assert.throws(() => { WithProcess(); }, /2 arguments/);
-      assert.throws(() => { WithProcess("node.exe"); }, /2 arguments/);
+      assert.throws(() => { WithProcess('node.exe'); }, /2 arguments/);
       assert.throws(() => { WithProcess(0, () => {}); }, /process name/);
-      assert.throws(() => { WithProcess("node.exe", 0); }, /callback function/);
+      assert.throws(() => { WithProcess('node.exe', 0); }, /callback function/);
     });
     it('should find a process by name and invoke callback', done => {
-      WithProcess("node.exe", () => {
+      WithProcess('node.exe', () => {
         done();
         return true;
       });
     });
     it('should provide the callback with a process object', done => {
-      WithProcess("node.exe", process => {
+      WithProcess('node.exe', process => {
         assert(process.getModuleBase);
         assert(process.readMemory);
         
@@ -28,7 +28,7 @@ describe('window-process', () => {
     });
     it('should not allow references to persist beyond scope of callback', () => {
       let leakedProcess;
-      WithProcess("node.exe", process => {
+      WithProcess('node.exe', process => {
         leakedProcess = process;
         return true;
       });
@@ -40,8 +40,8 @@ describe('window-process', () => {
     });
     describe('process.getModuleBase', () => {
       it('should find a well-known module offset', done => {
-        WithProcess("node.exe", process => {
-          assert(process.getModuleBase("ntdll.dll") > 0);
+        WithProcess('node.exe', process => {
+          assert(process.getModuleBase('ntdll.dll') > 0);
           
           done();
           return true;
@@ -50,8 +50,8 @@ describe('window-process', () => {
     });
     describe('process.readMemory', () => {
       it('should return a buffer', done => {
-        WithProcess("node.exe", process => {
-          const base = process.getModuleBase("ntdll.dll");
+        WithProcess('node.exe', process => {
+          const base = process.getModuleBase('ntdll.dll');
           const buffer = process.readMemory(base, 4);
           
           assert(buffer instanceof Buffer);
